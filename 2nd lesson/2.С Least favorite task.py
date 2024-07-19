1b263df8c941b3413fa8d0a7e568eef586e0c881
Эльмира Низамова <elmira.nizamova@gmail.com>
2024-07-19 16:33:56 +0300

    Create 2.С Least favorite task.py

---
+++ 2nd lesson/2.С Least favorite task.py
@@ -0,0 +1,56 @@
+"""
+C. Нелюбимое дело
+
+Нужно написать только функцию, которая принимает на вход голову списка и номер удаляемого элемента и возвращает голову
+обновлённого списка.
+
+Формат ввода
+Функция принимает голову списка и индекс элемента, который надо удалить (нумерация с нуля). Список содержит не более
+
+Формат вывода
+Верните голову списка, в котором удален нужный элемент.
+"""
+
+import os
+
+LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+
+if LOCAL:
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
+            self.next_item = next_item
+
+
+def solution(node, idx):
+    """delete random element"""
+    # def insert_node(head, index, value):
+    #     new_node = Node(value)
+    # if index == 0:
+    #         new_node.next = head
+    #             return new_node
+    while idx:
+        node = node.next
+        idx -= 1
+
+    previous_node = get_node_by_index(head, index-1)
+    new_node.next = previous_node.next
+    previous_node.next = new_node
+    return head
+
+
+def test():
+    node3 = Node("node3", None)
+    node2 = Node("node2", node3)
+    node1 = Node("node1", node2)
+    node0 = Node("node0", node1)
+    new_head = solution(node0, 1)
+    assert new_head is node0
+    assert new_head.next_item is node2
+    assert new_head.next_item.next_item is node3
+    assert new_head.next_item.next_item.next_item is None
+    # result is node0 -> node2 -> node3
+
+
+if __name__ == '__main__':
+    test()

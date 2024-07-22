efd2b4439f3827aa1591b6abea575b7411651af5
Эльмира Низамова <elmira.nizamova@gmail.com>
2024-07-22 16:49:07 +0300

    Update 2.С Least favorite task.py

--- 2nd lesson/2.С Least favorite task.py
+++ 2nd lesson/2.С Least favorite task.py
@@ -11,45 +11,47 @@ C. Нелюбимое дело
 Верните голову списка, в котором удален нужный элемент.
 """
 
-import os
 
-LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+class Node:
+    def __init__(self, value, next_item=None):
+        self.value = value
+        self.next_item = next_item
 
-if LOCAL:
-    class Node:
-        def __init__(self, value, next_item=None):
-            self.value = value
-            self.next_item = next_item
 
+def get_node_by_index(node, idx):
+    while idx: # это связный список и мы не можем обратить как к элементу массива по индексу, нам нужно дойти до нужного элемента
+        node = node.next_item
+        idx -= 1
+    return node
 
 def solution(node, idx):
-    """delete random element"""
-    while idx:
-        node = node.next
-        idx -= 1
-
-    previous_node = get_node_by_index(head, index-1)
-
-    new_node.next = previous_node.next
-    previous_node.next = new_node
-
-    previous_node =
-
-    return head
-
-
-def test():
-    node3 = Node("node3", None)
-    node2 = Node("node2", node3)
-    node1 = Node("node1", node2)
-    node0 = Node("node0", node1)
-    new_head = solution(node0, 1)
-    assert new_head is node0
-    assert new_head.next_item is node2
-    assert new_head.next_item.next_item is node3
-    assert new_head.next_item.next_item.next_item is None
-    # result is node0 -> node2 -> node3
+    if idx == 0:
+        node = node.next_item
+        return (node)
+    previous_node = get_node_by_index(node, idx-1)
+    deleted_node = get_node_by_index(node, idx)
+    if deleted_node.next_item is None:
+        previous_node.next_item = None
+    previous_node.next_item = deleted_node.next_item
+    while node:
+        print(node.value, end="\n")
+        node = node.next_item
+    # print(node)
 
 
-if __name__ == '__main__':
-    test()
+# def test():
+node3 = Node("node3", None)
+node2 = Node("node2", node3)
+node1 = Node("node1", node2)
+node0 = Node("node0", node1)
+#     new_head =
+solution(node0, 2)
+#     assert new_head is node0
+#     assert new_head.next_item is node2
+#     assert new_head.next_item.next_item is node3
+#     assert new_head.next_item.next_item.next_item is None
+#     # result is node0 -> node2 -> node3
+#
+#
+# if __name__ == '__main__':
+#     test()

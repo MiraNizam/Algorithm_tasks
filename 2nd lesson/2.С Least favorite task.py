ddc289082892c197cfb1f9a03980b09724a488ff
Эльмира Низамова <elmira.nizamova@gmail.com>
2024-07-24 17:29:58 +0300

    Update 2.С Least favorite task.py

--- 2nd lesson/2.С Least favorite task.py
+++ 2nd lesson/2.С Least favorite task.py
@@ -13,13 +13,16 @@ C. Нелюбимое дело
 
 
 class Node:
-    def __init__(self, value, next_item=None):
+    def __init__(self, value=None, next_item=None):
         self.value = value
         self.next_item = next_item
 
+    def __str__(self):
+        return self.value
+
 
 def get_node_by_index(node, idx):
-    while idx: # это связный список и мы не можем обратить как к элементу массива по индексу, нам нужно дойти до нужного элемента
+    while idx:
         node = node.next_item
         idx -= 1
     return node
@@ -29,27 +32,22 @@ def solution(node, idx):
     if idx == 0:
         node = node.next_item
         return node
+
     previous_node = get_node_by_index(node, idx-1)
     deleted_node = get_node_by_index(node, idx)
     if deleted_node.next_item is None:
         previous_node.next_item = None
-    previous_node.next_item = deleted_node.next_item
-    print(node)
+    else:
+        previous_node.next_item = deleted_node.next_item
+    return node
 
 
-# def test():
-node3 = Node("node3", None)
+node6 = Node("node6", None)
+node5 = Node("node5", node6)
+node4 = Node("node4", node5)
+node3 = Node("node3", node4)
 node2 = Node("node2", node3)
 node1 = Node("node1", node2)
 node0 = Node("node0", node1)
-#     new_head =
-solution(node0, 2)
-#     assert new_head is node0
-#     assert new_head.next_item is node2
-#     assert new_head.next_item.next_item is node3
-#     assert new_head.next_item.next_item.next_item is None
-#     # result is node0 -> node2 -> node3
-#
-#
-# if __name__ == '__main__':
-#     test()
+
+print(solution(node0, 4))

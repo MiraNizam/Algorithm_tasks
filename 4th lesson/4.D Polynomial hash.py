c1f506d20d2048b41ef9ed3025b94825fa9a28f4
MiraNizam <82273561+MiraNizam@users.noreply.github.com>
2024-08-23 16:14:54 +0300

    Create 4.D Polynomial hash.py

---
+++ 4th lesson/4.D Polynomial hash.py
@@ -0,0 +1,20 @@
+"""
+D. Полиномиальный хеш
+"""
+a = int(input())
+m = int(input())
+s = input()
+
+
+def polynominal_hash(a, m, s):
+    a_in_power = 1
+    h = 0
+
+    for i in s[::-1]:
+        h = (h + ord(i)*a_in_power) % m
+        a_in_power = a*a_in_power % m # вариант a_in_power *= a % m будет забирать доп.время
+    return h
+
+
+if __name__ == "__main__":
+    print(polynominal_hash(a, m, s))
